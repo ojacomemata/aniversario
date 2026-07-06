b61b23cbafd54452d6692542bf53b0a77bdc4f86
Octavio Jacome Mata <o.jacome.mata@gmail.com>
2026-07-06 01:18:46 -0600

    Estructura final

--- js/main.js
+++ js/main.js
@@ -11,3 +11,15 @@ document.addEventListener("DOMContentLoaded", () => {
     `);
 
 });
+
+const photos = document.querySelectorAll(".polaroid img");
+
+photos.forEach(photo=>{
+
+    photo.addEventListener("click",()=>{
+
+        openViewer(photo.dataset.full);
+
+    });
+
+});
